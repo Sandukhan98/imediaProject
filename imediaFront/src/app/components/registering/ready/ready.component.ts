57bfe021ba1426757e4ef5cd31e5048e2c8138fb
spiksla <spiksla@spiksla.spiksla>
2021-11-08 17:33:30 +0100

    change back address

--- imediaFront/src/app/components/registering/ready/ready.component.ts
+++ imediaFront/src/app/components/registering/ready/ready.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ready',
@@ -9,13 +10,17 @@ import { ActivatedRoute } from '@angular/router';
 export class ReadyComponent implements OnInit {
   firstName = "";
   lastName= "";
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.activatedRoute.queryParams.subscribe(params => {
       this.firstName = params.firstName;
       this.lastName = params.lastName;
     });
+
+    setTimeout(() => {
+      this.router.navigate(['/']);
+  }, 5000);  //5s
   }
   
 }
