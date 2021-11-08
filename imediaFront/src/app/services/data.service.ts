73bedbfd34c86491c3086219c0bb5b878073b5fc
spiksla <spiksla@spiksla.spiksla>
2021-11-08 16:40:29 +0100

    change back address

--- imediaFront/src/app/services/data.service.ts
+++ imediaFront/src/app/services/data.service.ts
@@ -13,22 +13,22 @@ export class DataService {
 
   getCompanyInfo(tva:any): Observable<any> {
     console.log(tva)
-    return this.http.post('http://localhost:3000/tva', tva)
+    return this.http.post('http://192.168.1.60:3000/tva', tva)
   }
 
   addCustomer(info:any):Observable<any> {
     console.log(info)
-    return this.http.post('http://localhost:3000/addCustomer', info)
+    return this.http.post('http://192.168.1.60:3000/addCustomer', info)
   }
 
   verifyExistence(email:any): Observable<any> {
     console.log(email)
-    return this.http.post('http://localhost:3000/exist', email)
+    return this.http.post('http://192.168.1.60:3000/exist', email)
   }
 
   addWorkorder(customerID:any): Observable<any> {
     console.log(customerID)
-    return this.http.post('http://localhost:3000/workorder', customerID)
+    return this.http.post('http://192.168.1.60:3000/workorder', customerID)
   }
 
 }
